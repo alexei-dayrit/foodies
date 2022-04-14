09f62c9c76f38476eec7dda8109e8bc522c655c1
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-04-14 05:04:34 +0000

    Added Post component. #3

---
+++ client/components/post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userPosts: []
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <p></p>
+      </>
+    );
+  }
+}
--- client/components/profile.jsx
+++ client/components/profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Post from './post';
 
 export default class Profile extends React.Component {
   constructor(props) {
@@ -40,7 +41,7 @@ export default class Profile extends React.Component {
               </div>
             </div>
           </div>
-          {/* Post component goes here */}
+          <Post />
         </div>
 
       </>
