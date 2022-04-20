0bf1a9cfbe45af4d4faa4a24fcbd9fb34d0012b8
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-04-20 20:08:21 +0000

    Stored 'GET' request data in state. #13

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
   componentDidMount() {
     fetch('api/posts')
       .then(res => res.json())
       .then(posts => {
-
+        this.setState({ posts: posts });
+        console.log('posts State:', this.state.posts);
       })
       .catch(err => console.error(err));
   }
