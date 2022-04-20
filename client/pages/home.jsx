992c3c1df90f1a46280ec3378acd9322ac76279d
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-04-20 20:06:02 +0000

    Updated Home component to fetch 'GET' request when componentDidMount(). #13

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-      <div className='text-3xl text-center mt-4'>HOME PAGE</div>
-    </>
-  );
+export default class Home extends React.Component {
+  componentDidMount() {
+    fetch('api/posts')
+      .then(res => res.json())
+      .then(posts => {
+
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <>
+        <div className='text-3xl text-center mt-4'>HOME PAGE</div>
+      </>
+    );
+  }
 }
