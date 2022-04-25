dd8f60d2257dd545aaa64c8cfcbc99079640fda3
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-04-25 05:59:40 +0000

    Updated data.sql with better dummy data. #18

--- database/data.sql
+++ database/data.sql
@@ -1,20 +1,42 @@
 -- DANGER: this is NOT how to do it in the real world.
 -- You should NEVER EVER check user data into Git!
 
+-- SAMPLE USER1
 insert into "users" ("userId", "username", "profilePhotoUrl", "hashedPassword", "followerCount", "followingCount", "postCount")
-     values (100, 'anonymous1', null, 'pass1', 0, 0, 0);
+     values (100, 'sushi_lover', 'image-1650864757312.jpeg', 'pass1', 2, 1, 3);
 
+insert into "posts" ("userId", "postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (100, 100, 'Celebrated my birthday with delicious sushi!', 'image-1650860094759.jpeg', 'San Francisco, CA', true, '2022-03-21 11:30:10');
+
+insert into "posts" ("userId", "postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (100, 101, 'This is great.', 'image-1650862677041.JPG', 'Irvine, CA', true, '2022-04-21 12:30:10');
+
+insert into "posts" ("userId", "postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (100, 102, 'MOAR Sushi haha.', 'image-1650861360080.jpeg', 'Concord, CA', true, '2022-04-23 14:30:10');
+
+
+-- SAMPLE USER2
 insert into "users" ("userId", "username",  "profilePhotoUrl", "hashedPassword", "followerCount", "followingCount", "postCount")
-     values (101, 'anonymous2', null, 'pass2', 0, 0, 0);
+     values (101, 'casual_cook', 'image-1650864705652.jpeg', 'pass2', 2, 2, 3);
 
-insert into "posts" ("postId", "caption", "imageUrl", "location", "userId", "isBought")
-     values (100, 'caption1', null, 'location1', 100, true);
+insert into "posts" ("userId","postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (101, 107, 'First post! Upgraded ramen noodles.', 'image-1650859980943.jpeg', 'Vallejo, CA', false, '2022-04-17 18:30:10');
 
-insert into "posts" ("postId", "caption", "imageUrl", "location", "userId", "isBought", "createdAt")
-     values (101, 'caption2', 'image-1650859980943', 'location2', 100, false, '2022-04-21 12:30:10');
+insert into "posts" ("userId","postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (101, 108, 'Made some fish and chips with some sides.', 'image-1650861154878.jpeg', 'Vallejo, CA', false, '2022-04-19 18:30:10');
 
-insert into "posts" ("postId", "caption", "imageUrl", "location", "userId", "isBought")
-     values (102, 'caption3', 'image-1649888023634.jpeg', 'location3', 101, false);
+insert into "posts" ("userId","postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (101, 109, 'Seafood boil', 'image-1650861103970.JPG', 'Vallejo, CA', false, '2022-04-23 15:30:10');
+
+-- SAMPLE USER3
+insert into "users" ("userId", "username", "profilePhotoUrl", "hashedPassword", "followerCount", "followingCount", "postCount")
+     values (102, 'varietyEater', null, 'pass1', 2, 1, 2);
+
+insert into "posts" ("userId","postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (102, 110, 'Perfect for a cold day!', 'image-1650862870268.jpeg', 'Irvine, CA', true, '2022-04-22 15:30:10');
+
+insert into "posts" ("userId","postId", "caption", "imageUrl", "location", "isBought", "createdAt")
+     values (102, 111, 'Just get mild.', 'image-1650863415145.jpeg', 'Irvine, CA', true, '2022-04-23 15:30:10');
 
 insert into "comments" ("userId", "comment", "postId")
      values (101, 'WOW', 100);
@@ -24,4 +46,3 @@ insert into "comments" ("userId", "comment", "postId")
 
 insert into "comments" ("userId", "comment", "postId")
      values (101, 'That looks so good! Where did you get this?', 101);
--- 2022-04-12T18:44:59.581239Z
