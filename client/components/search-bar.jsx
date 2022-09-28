aa7f0fc474591d72d2fbab6ce343fb76ed523d82
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-09-28 04:17:57 +0000

    Added onClick listener to input field and useeRef to handle blur. #53

--- client/components/search-bar.jsx
+++ client/components/search-bar.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import SearchIcon from './svg-assets/search-icon';
 
 const SearchBar = ({ posts }) => {
 
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isSearching, setIsSearching] = useState(true);
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    setIsSearching(prev => !prev);
+  };
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+    inputRef.current?.blur();
+  };
+
   return (
     <div>
-      <SearchIcon/>
+      {isSearching
+        ? <form onSubmit={handleSearchSubmit} className='relative'>
+          <input ref={inputRef} type='text' className='border rounded-md border-black w-56 p-1' />
+          <div onClick={handleClick} className='absolute right-1 top-1'>
+            <SearchIcon />
+          </div>
+        </form>
+        : <h2 onClick={handleClick}>Search</h2>
+      }
     </div>
   );
 };
